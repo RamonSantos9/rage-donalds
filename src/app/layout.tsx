e9ca11cd05ebc2a0f05706176d3c065b43370462
RamonSantos9 <ramonfishh@gmail.com>
2025-02-23 23:44:01 -0300

    feat: add Label and Toaster components, update layout and product handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
+import { Toaster } from "@/components/ui/sonner";
+
 import { CartProvider } from "./[slug]/menu/contexts/cart";
 
 const poppins = Poppins({
@@ -11,8 +13,8 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "FSW Donalds",
-  description: "Bora finalizar esse projeto lindo!",
+  title: "RAGE Donalds",
+  description: "",
 };
 
 export default function RootLayout({
@@ -24,6 +26,8 @@ export default function RootLayout({
     <html lang="pt-br">
       <body className={`${poppins.className} antialiased`}>
         <CartProvider>{children}</CartProvider>
+
+        <Toaster />
       </body>
     </html>
   );
